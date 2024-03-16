e861b9487d8ee56a03aae35cbdaa81dc17ab2732
Aaron <apeckham@gmail.com>
2024-03-16 06:40:28 +0000

    move to package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=["click>=7.0", "tiktoken>=0.2.0"],
     entry_points={
         "console_scripts": [
-            "token-tail=main:cli",
+            "token-tail=token_tail.main:cli",
         ],
     },
 )
