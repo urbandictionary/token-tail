92e01ec50166b725ea5d481850d7a362d7690657
Aaron <apeckham@gmail.com>
2024-03-16 06:30:30 +0000

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="token-tail",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["click>=7.0", "tiktoken>=0.2.0"],
+    entry_points={
+        "console_scripts": [
+            "your_script=head_tail:cli",
+        ],
+    },
+)
