51aaa8bf66fcde0ec319b24a95116a4c956fe2df
Aaron <apeckham@gmail.com>
2024-03-17 04:47:43 +0000

    add split command

--- token_tail/main.py
+++ token_tail/main.py
@@ -1,11 +1,13 @@
 import click
 import sys
 import tiktoken
+import string
+import os
 
 
-def run(token_count, model, input_stream, tail):
+def run(token_count, model, file, tail):
     enc = tiktoken.encoding_for_model(model)
-    tokens = enc.encode(input_stream.read())
+    tokens = enc.encode(file.read())
     selected_tokens = tokens[-token_count:] if tail else tokens[:token_count]
     click.echo(enc.decode(selected_tokens))
 
@@ -34,3 +36,33 @@ def add_command(func, name):
 
 add_command(run, "head")
 add_command(run, "tail")
+
+
+@cli.command
+@click.option(
+    "-n", "--token-count", default=100, help="Maximum token length for each split part."
+)
+@click.option(
+    "-m", "--model", default="gpt-4", help="GPT model to use for tokenization."
+)
+@click.option("--overwrite", is_flag=True, help="Overwrite existing files.")
+@click.argument("file", type=click.File("r"), default=sys.stdin)
+def split(token_count, model, file, overwrite):
+    enc = tiktoken.encoding_for_model(model)
+    content = file.read()
+    tokens = enc.encode(content)
+    prefix = "x"
+
+    for i, start in enumerate(range(0, len(tokens), token_count), start=0):
+        file_suffix = (string.ascii_lowercase + string.ascii_lowercase)[i // 26] + (
+            string.ascii_lowercase
+        )[i % 26]
+        filename = f"{prefix}{file_suffix}"
+
+        if not overwrite and os.path.exists(filename):
+            raise FileExistsError(
+                f"File {filename} already exists. Use --overwrite to ignore."
+            )
+
+        with open(filename, "w") as file:
+            file.write(enc.decode(tokens[start : start + token_count]))
